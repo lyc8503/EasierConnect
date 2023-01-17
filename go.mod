e4fbf35589bd759943b188ce4ec880c6843bf248
lyc8503 <admin@lyc8503.site>
2023-01-17 13:46:33 +0800

    ping!

---
+++ go.mod
@@ -0,0 +1,12 @@
+module EasierConnect
+
+go 1.19
+
+require github.com/refraction-networking/utls v1.2.0
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
---
+++ test.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+	"log"
+	"net"
+	"os"
+	"time"
+
+	tls "github.com/refraction-networking/utls"
+)
+
+func dumpHex(buf []byte) {
+	stdoutDumper := hex.Dumper(os.Stdout)
+	defer stdoutDumper.Close()
+	stdoutDumper.Write(buf)
+}
+
+func tlsConn(server string) (*tls.UConn, error) {
+	// dial vpn server
+	dialConn, err := net.Dial("tcp", server)
+	if err != nil {
+		return nil, err
+	}
+	log.Println("socket: connected to: ", dialConn.RemoteAddr())
+
+	// using uTLS to construct a weird TLS Client Hello (required by Sangfor)
+	// The VPN and HTTP Server share port 443, Sangfor uses a special SessionId to distinguish them. (which is very stupid...)
+	conn := tls.UClient(dialConn, &tls.Config{InsecureSkipVerify: true}, tls.HelloCustom)
+
+	random := make([]byte, 32)
+	_, err = rand.Read(random) // Ignore the err
+	conn.SetClientRandom(random)
+	conn.SetTLSVers(tls.VersionTLS11, tls.VersionTLS11, []tls.TLSExtension{})
+	conn.HandshakeState.Hello.Vers = tls.VersionTLS11
+	conn.HandshakeState.Hello.CipherSuites = []uint16{tls.TLS_RSA_WITH_RC4_128_SHA, tls.FAKE_TLS_EMPTY_RENEGOTIATION_INFO_SCSV}
+	conn.HandshakeState.Hello.CompressionMethods = []uint8{0}
+	conn.HandshakeState.Hello.SessionId = []byte{'L', '3', 'I', 'P', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+	log.Println("tls: connected to: ", conn.RemoteAddr())
+	return conn, nil
+}
+
+func recvListen(conn *tls.UConn, token *[48]byte, ip *[4]byte) {
+	// RECV STREAM START
+	message := []byte{0x06, 0x00, 0x00, 0x00}
+	message = append(message, token[:]...)
+	message = append(message, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
+	message = append(message, ip[:]...)
+
+	n, err := conn.Write(message)
+
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("recv handshake: wrote %d bytes", n)
+	dumpHex(message[:n])
+
+	reply := make([]byte, 1500)
+	n, err = conn.Read(reply)
+	log.Printf("recv handshake: read %d bytes", n)
+	dumpHex(reply[:n])
+
+	if reply[0] != 0x01 {
+		panic("unexpected recv handshake reply.")
+	}
+
+	for true {
+		n, err = conn.Read(reply)
+		log.Printf("recv: read %d bytes", n)
+		dumpHex(reply[:n])
+
+		time.Sleep(time.Second)
+	}
+}
+
+func send() {
+
+}
+
+func main() {
+
+	server := "vpn.nju.edu.cn:443"
+	token := []byte("\x66\x65\x32\x30" +
+		"\x62\x33\x30\x63\x30\x62\x39\x31\x62\x65\x65\x37\x37\x38\x30\x61" +
+		"\x66\x64\x39\x37\x30\x30\x38\x31\x31\x63\x37\x00\x64\x31\x39\x34" +
+		"\x30\x33\x65\x31\x64\x34\x62\x66\x32\x32\x32\x30")
+
+	ip := []byte("\x66\x28\x1d\xac")
+
+	conn, err := tlsConn(server)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	go recvListen(conn, (*[48]byte)(token), (*[4]byte)(ip))
+
+	// tlsConn for sending data
+	conn, err = tlsConn(server)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+
+	// SEND STREAM START
+	message := []byte{0x08, 0x00, 0x00, 0x00}
+	message = append(message, token[:]...)
+	message = append(message, []byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}...)
+	message = append(message, ip[:]...)
+
+	n, err := conn.Write(message)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("send handshake: wrote %d bytes", n)
+	dumpHex(message[:n])
+
+	reply := make([]byte, 1500)
+	n, err = conn.Read(reply)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("send handshake: read %d bytes", n)
+	dumpHex(reply[:n])
+
+	if reply[0] != 0x02 {
+		panic("unexpected send handshake reply.")
+	}
+
+	message2 := "\x45\x00\x00\x3c\x01\x6f\x00\x00\x80\x01\x8e\x40\xac\x1d\x28\x66" +
+		"\x72\xd4\x63\xba\x08\x00\x49\x2c\x00\x01\x04\x2f\x61\x62\x63\x64" +
+		"\x65\x66\x67\x68\x69\x6a\x6b\x6c\x6d\x6e\x6f\x70\x71\x72\x73\x74" +
+		"\x75\x76\x77\x61\x62\x63\x64\x65\x66\x67\x68\x69"
+	n, err = io.WriteString(conn, message2)
+	log.Printf("send: wrote %d bytes", n)
+	dumpHex([]byte(message2[:n]))
+
+	for true {
+		time.Sleep(time.Second)
+	}
+
+	// // HANDSHAKE?
+	// // message = message + "\x05\x00\x00\x00" + token + "\x00\x00\x00\x00\x00\x00\x00\x00" + ip
+
+	// // HEARTBEAT?
+	// // message = message + "\x03\x00\x00\x00" + token + "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+
+}
